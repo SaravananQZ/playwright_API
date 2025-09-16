50b4e7d9bf23df3721d6a936d2724a668cf8aaa3
saravanansubramani-rimsys <112157986+saravanansubramani-rimsys@users.noreply.github.com>
2025-09-16 12:27:07 +0530

    First commit

---
+++ common/env.ts
@@ -0,0 +1,6 @@
+export default class ENV{
+  public static API_URL = process.env.BASE_URL
+  public static TR_URL = process.env.TR_URL
+  public static TR_USERNAME = process.env.USERNAME
+  public static TR_PASSWORD = process.env.PASSWORD
+}
---
+++ common/excelconnection.ts
@@ -0,0 +1,42 @@
+import * as XLSX from 'xlsx';
+
+
+export  class xlscls{
+    excelfile:"";
+    sheetname:"";
+    rows:any;
+    constructor(filename,sheetname){
+        this.excelfile = filename;
+        this.sheetname = sheetname;
+        this.rows=null;
+
+    }
+   getRowsExcel(){
+    // Load workbook
+        const workbook = XLSX.readFile(this.excelfile);
+
+        // Select first sheet
+        const sheetName = workbook.SheetNames[0];
+        const sheet = workbook.Sheets[this.sheetname];
+
+        // Convert to JSON
+        this.rows = XLSX.utils.sheet_to_json(sheet);
+        return this.rows;
+   }
+   getTestcaseDetails(tcname,testtype='API'){
+    const filtered = this.rows.filter(row => row.TC_Name === tcname);
+    const result = filtered.reduce((acc, row) => {
+        acc[row.Attribute] = row.Value;
+        return acc;
+      }, {} as Record<string, string | number>);
+    return result;
+   }
+}
+
+
+
+
+// // Filter like pandas
+// const filtered = rows.filter(row => row.Status === 'Fail');
+
+// console.log(filtered);
